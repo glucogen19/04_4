116f81f619ac5d9be93cbfa02454a971557a94f9
glucogen19 <98939682+glucogen19@users.noreply.github.com>
2023-06-15 09:52:18 +0400

    Считывание данных из файла построчно

--- m.cpp
+++ m.cpp
@@ -31,4 +31,24 @@ int main(){
         std::cout << "Failed to open the file." << std::endl;
         return 1;
     }
+	
+	std::string line;
+    while (std::getline(inputFile, line)) {
+        std::stringstream ss(line);
+        std::string name, author;
+        int count;
+
+        if (std::getline(ss, name, ';') && std::getline(ss, author, ';') && (ss >> count)) {
+            TBook book;
+            book.setName(name);
+            book.setAuthor(author);
+            book.setCount(count);
+            books.push_back(book);
+        }
+        else {
+            std::cout << "Invalid data: " << line << std::endl;
+        }
+    }
+
+    inputFile.close();
 }
