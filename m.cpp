9a48c36f7f18190f80a990178b9e862afecb7fa7
glucogen19 <98939682+glucogen19@users.noreply.github.com>
2023-06-15 09:23:10 +0400

    Создание геттеров и сеттеров для работы с private

--- m.cpp
+++ m.cpp
@@ -9,6 +9,14 @@ class TBook {
     std::string Name;
     std::string Author;
     int Count;
+	public:
+    std::string getName() const { return Name; }
+    std::string getAuthor() const { return Author; }
+    int getCount() const { return Count; }
+
+    void setName(const std::string& name) { Name = name; }
+    void setAuthor(const std::string& author) { Author = author; }
+    void setCount(int count) { Count = count; }
 };
 
 int main(){}
